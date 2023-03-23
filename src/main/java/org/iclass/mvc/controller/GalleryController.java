c255c28acf3c42ed4d8f1eec051a1aff39a58d2e
kimsewhee <koreait.sec2020@gmail.com>
2023-03-23 11:19:54 +0900

    4_service 파일업로드

--- src/main/java/org/iclass/mvc/controller/GalleryController.java
+++ src/main/java/org/iclass/mvc/controller/GalleryController.java
@@ -1,5 +1,7 @@
 package org.iclass.mvc.controller;
 
+import org.iclass.mvc.dto.Gallery;
+import org.iclass.mvc.service.GalleryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,6 +9,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class GalleryController {
 
+	private GalleryService service;
+	
+	public GalleryController(GalleryService service) {
+		this.service=service;
+	}
 	
 	@GetMapping("/gallery")
 	public void gallery() {		//화면 : 파일명은 gallery.html
@@ -14,8 +21,8 @@ public class GalleryController {
 	}
 	
 	@PostMapping("/gallery")
-	public String save() {
-		
+	public String save(Gallery vo) {
+		int count = service.save(vo);
 		return "redirect:gallery";
 	}
 }
