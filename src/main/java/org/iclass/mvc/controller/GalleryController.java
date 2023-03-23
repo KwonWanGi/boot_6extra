1a3ae565670c53c605fb36b6a702c5a7b29d5819
kimsewhee <koreait.sec2020@gmail.com>
2023-03-23 09:47:14 +0900

    1_초기화

---
+++ src/main/java/org/iclass/mvc/controller/GalleryController.java
@@ -0,0 +1,21 @@
+package org.iclass.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class GalleryController {
+
+	
+	@GetMapping("/gallery")
+	public void gallery() {		//화면 : 파일명은 gallery.html
+		
+	}
+	
+	@PostMapping("/gallery")
+	public String save() {
+		
+		return "redirect:gallery";
+	}
+}
